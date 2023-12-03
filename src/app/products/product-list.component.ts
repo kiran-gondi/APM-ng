ef8d09533dc9cf9dabd8ee5a1a59be1569b5c751
kiran.gondi <kiran.gondi@planview.com>
2023-12-03 21:48:16 +0530

    Chapter 7 - More on Components

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,16 +1,29 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { IProduct } from "./product";
 
 @Component({
   selector: 'pm-products',
-  templateUrl: './product-list.component.html'
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+    
   pageTitle = 'Product List';
   imageWidth: number = 50;
   imageMargin = 2;
   showImage: boolean = false;
-  listFilter: string = 'cart';
-  products: any[] = [
+  
+    private _listFilter: string = '';
+    get listFilter() : string{
+        return this._listFilter;
+    }
+    set listFilter(value: string) {
+        this._listFilter = value;
+        console.log('In Setter: ', value);
+        this.filteredProducts = this.performFilter(value);
+    }
+
+    products: IProduct[] = [
     {
       "productId": 2,
       "productName": "Garden Cart",
@@ -33,7 +46,17 @@ export class ProductListComponent {
     }
   ];
 
+  filteredProducts: IProduct[] = [];   
+ performFilter(filterBy: string): IProduct[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter((product: IProduct) => product.productName.toLocaleLowerCase().includes(filterBy));
+}
+
   toggleImage() : void{
     this.showImage = !this.showImage;
+  }
+
+  ngOnInit(): void {
+    this.listFilter = 'cart';
   }
 }
